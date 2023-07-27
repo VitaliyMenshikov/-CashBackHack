107f97c56af0d85702ddcd82d419a3934054ecaf
vitaliy.m <vitek1993-11@mail.ru>
2023-07-27 11:52:49 +0300

    testNG

--- src/test/java/ru/netologu/CashbackHackServiceTest.java
+++ src/test/java/ru/netologu/CashbackHackServiceTest.java
@@ -1,4 +1,53 @@
+package ru.netologu;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 400;
+        int actual = service.remain(amount);
+        int expected = 600;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain4() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1800;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
